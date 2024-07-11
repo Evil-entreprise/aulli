318f2c5bc75635c0356c169686f233412667c072
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-07-11 17:12:08 +0100

    feat: added database url and email smtp server url

--- env.ts
+++ env.ts
@@ -4,9 +4,12 @@ import * as z from 'zod';
 const env = createEnv({
   server: {
     AUTH_DRIZZLE_URL: z.string().url(),
+    DATABASE_URL: z.string().url(),
     AUTH_SECRET: z.string().min(1),
     AUTH_GOOGLE_ID: z.string().min(1),
     AUTH_GOOGLE_SECRET: z.string().min(1),
+    EMAIL_SERVER: z.string().min(1),
+    EMAIL_FROM: z.string().min(1),
   },
   client: {},
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
@@ -15,6 +18,9 @@ const env = createEnv({
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+    EMAIL_SERVER: process.env.EMAIL_SERVER,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   // experimental__runtimeEnv: {
