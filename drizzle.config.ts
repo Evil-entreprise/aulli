075770cbe357974ac4647d3cb91201bd2c64f39f
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-07-11 17:14:41 +0100

    feat: added config and dialect

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'drizzle-kit';
 import env from '~env';
+import type { Config } from 'drizzle-kit';
+
+type Dialect = 'postgresql' | 'mysql' | 'sqlite';
 export default defineConfig({
+  dialect: 'postgresql' as Dialect,
   schema: './drizzle/schema.ts',
-  out: './database/migrations',
-  dialect: 'postgresql',
+  out: './drizzle/migrations',
   dbCredentials: {
-    url: env['AUTH_DRIZZLE_URL'],
+    url: env.DATABASE_URL!,
+    ssl: false,
   },
   verbose: true,
   strict: true,
-});
+}) satisfies Config;
