91225e2714da9cb92aaef418e4537dcb5b40a799
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-07-12 09:22:33 +0100

    fix: removed unused env variables

--- env.ts
+++ env.ts
@@ -3,21 +3,13 @@ import * as z from 'zod';
 
 const env = createEnv({
   server: {
-    AUTH_DRIZZLE_URL: z.string().url(),
     DATABASE_URL: z.string().url(),
-    AUTH_SECRET: z.string().min(1),
-    AUTH_GOOGLE_ID: z.string().min(1),
-    AUTH_GOOGLE_SECRET: z.string().min(1),
     EMAIL_SERVER: z.string().min(1),
     EMAIL_FROM: z.string().min(1),
   },
   client: {},
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
-    AUTH_DRIZZLE_URL: process.env.DATABASE_URL,
-    AUTH_SECRET: process.env.AUTH_SECRET,
-    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
-    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
     EMAIL_SERVER: process.env.EMAIL_SERVER,
     EMAIL_FROM: process.env.EMAIL_FROM,
     DATABASE_URL: process.env.DATABASE_URL,
