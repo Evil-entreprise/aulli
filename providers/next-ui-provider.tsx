b9067828ac434001addb0994b93a2e4b66e684e1
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-07-12 09:22:03 +0100

    feat: added package next ui. framer motion ...
    upgrade: next js 15 to next js 15 canary - test

---
+++ providers/next-ui-provider.tsx
@@ -0,0 +1,6 @@
+'use client';
+import React, { ReactNode } from 'react';
+import { NextUIProvider } from '@nextui-org/react';
+export default function UIProvider({ children }: { children: ReactNode }) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
