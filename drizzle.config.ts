a8578b533ef80117aef293b05b6cda5eb396a241
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-07-12 09:21:09 +0100

    fix: dialect error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
-import env from '~env';
 import type { Config } from 'drizzle-kit';
 
-type Dialect = 'postgresql' | 'mysql' | 'sqlite';
 export default defineConfig({
-  dialect: 'postgresql' as Dialect,
   schema: './drizzle/schema.ts',
   out: './drizzle/migrations',
   dbCredentials: {
-    url: env.DATABASE_URL!,
-    ssl: false,
+    url: process.env.DATABASE_URL!,
+    // ssl: true,
   },
+  dialect: 'postgresql',
   verbose: true,
   strict: true,
 }) satisfies Config;
