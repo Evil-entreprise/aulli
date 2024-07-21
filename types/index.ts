c17749c3dbec37710526cd989fc902fd4cfe5e6e
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-07-21 20:27:01 +0100

    feat added prop type for svg's

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,11 @@
 import { users } from '~drizzle/schema';
 import { InferSelectModel } from 'drizzle-orm';
+import * as z from 'zod';
+import type { SVGProps } from 'react';
+
+export type IconSvgProps = SVGProps<SVGSVGElement> & {
+  size?: number;
+};
 
 export type User = InferSelectModel<typeof users>;
 
