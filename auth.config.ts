c2725a8b868a4c8d46be1f41be7fab2b04ab15cc
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-07-30 23:20:22 +0100

    feat: moved drizzle adapter to auth.ts (resolving edge compile error for email template)

--- auth.config.ts
+++ auth.config.ts
@@ -1,25 +1,19 @@
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { eq } from 'drizzle-orm';
-import NextAuth from 'next-auth';
 import type { NextAuthConfig } from 'next-auth';
 import Google from 'next-auth/providers/google';
-import GoogleProvider from '@auth/core/providers/google';
 import Resend from 'next-auth/providers/resend';
-import { users } from '~database/models/users';
-import drizzle from '~drizzle';
-import env from '~env';
 import { sendVerificationRequest } from '~utils/auth/magic-links';
 
 export default {
-  adapter: DrizzleAdapter(drizzle),
   providers: [
-    GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID as string,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
     }),
     Resend({
-      apiKey: process.env.AUTH_RESEND_KEY,
-      from: env.EMAIL_FROM,
+      apiKey: process.env.AUTH_RESEND_KEY!,
+      from: process.env.EMAIL_FROM!,
       sendVerificationRequest,
     }),
   ],
@@ -41,30 +35,20 @@ export default {
       }
       return session;
     },
-   async redirect({ url, baseUrl }) {
-      if (url.startsWith('/')) {
-        return `${baseUrl}${url}`;
-      } else if (new URL(url).origin === baseUrl) {
-        return url;
-      }
+    async redirect({ url, baseUrl }) {
       return baseUrl;
     },
     async signIn({ user, account, profile, email, credentials }) {
-      if (user) {
-        const existingUser = await drizzle
-          .select()
-          .from(users)
-          .where(eq(users.email, user?.email!));
-
-        if (existingUser) {
-          return true;
-        }
-      }
       return true;
     },
     authorized({ request, auth }) {
       const { pathname } = request.nextUrl;
-      const openRoutes = ['/get-started', '/verify-request']; // specify routes you want to protect in this array
+      const openRoutes = [
+        '/get-started',
+        '/verify-request',
+        '/manifest.webmanifest',
+        '/icons',
+      ]; // specify routes you want to protect in this array
       if (!openRoutes.includes(pathname)) return !!auth;
       return true;
     },
@@ -73,6 +57,6 @@ export default {
     signIn: '/auth/get-started',
     newUser: '/auth/get-started',
     verifyRequest: '/auth/verify-request',
-    error:'/auth/failed-verification'
+    error: '/auth/failed-verification',
   },
 } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -1,11 +1,15 @@
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import NextAuth from 'next-auth';
 import config from '~auth.config';
 
+import drizzle from '~drizzle';
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
 } = NextAuth({
+  debug: true,
+  adapter: DrizzleAdapter(drizzle),
   ...config,
 });
