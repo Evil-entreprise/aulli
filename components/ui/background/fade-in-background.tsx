36c6dedcc6c5caa1bc9e32410f0ec77ba77ef9bc
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-08-08 03:54:32 +0100

    feat: added clx for className and cs for conditional styles

--- components/ui/background/fade-in-background.tsx
+++ components/ui/background/fade-in-background.tsx
@@ -1,18 +1,21 @@
 'use client';
 import { ReactNode } from 'react';
 import { motion } from 'framer-motion';
+import { cx } from '~utils/cx';
 
 export default function FadeInBackground({
   children,
+  clx,
 }: Readonly<{
   children: ReactNode;
+  clx?: string;
 }>) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.3, ease: [0.36, 0.44, 0.74, 0.75] }}
-      className='h-dvh w-screen flex items-center justify-center'
+      className={cx('h-dvh w-screen flex items-center justify-center', clx)}
     >
       {children}
     </motion.div>
