fee06e2fe8662ca114bea92e65d29b2ed6a63eca
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-08-08 16:48:32 +0100

    fix: removed experimental typedRoutes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	serverExternalPackages: ['@react-email/tailwind', '@react-email/components'],
-	experimental: {
-		typedRoutes: true
-	}
 };
 
 export default nextConfig;
