fcd572d4c32103ec4b9e9301e891b4906847a207
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-08-08 16:10:27 +0100

    feat: added experimental feature typed routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	serverExternalPackages: ['@react-email/tailwind', '@react-email/components'],
+	experimental: {
+		typedRoutes: true
+	}
+};
 
 export default nextConfig;
